3268299ed6f414a86c3c8476816e5a0d4f4c9dde
unknown <ecomje@gmail.com>
2018-11-09 14:25:00 -0800

    added create, update, and check parcel exist methods

--- controller/models/parcel.model.js
+++ controller/models/parcel.model.js
@@ -1,4 +1,4 @@
-import {inArray, removeArray} from '../helpers/helper';
+import {inArray, removeArray, generateID} from '../helpers/helper';
 
 export default class parcelOrder {
   constructor (parcels) {
@@ -19,6 +19,7 @@ export default class parcelOrder {
   getUserParcels (userid) {
     return this.parcels.find( parcels => parcels.userid == userid);
   }
+
   removeParcel (parcelId) {
     const deleteOrder= this.parcels.find(p => p.id === parseInt(parcelId))
     if (!deleteOrder){
@@ -30,10 +31,40 @@ export default class parcelOrder {
   }
 
   createOrder (order) {
-    let orderDetails = {};
-    for (let [key, value] of order) {
-      orderDetails[key] = value ;
+    let orderId = generateID(999);
+    if (this.orderExist(orderId)){
+      orderId = orderId * 2;
     }
-    this.parcels.push (orderDetails);
+
+    this.parcels.push ({
+      userid: order.userid,
+      id: orderId,
+      content: order.content,
+      pickup: order.pickup,
+      pickupcode: order.pickupcode,
+      destination: order.destination,
+      destinationcode: order.destinationcode,
+      weight: order.weight
+    });
+  }
+  updateParcel (id, field, value = null){
+    if (value !== null){
+      this.getSingleParcel(id).field = value;
+      return 'status has been updated';
+    }
+    const parcel = this.getSingleParcel (id);
+    parcel.destination = field.destination;
+    parcel.destinationcode = field.destinationcode;
+    return 'destination has been updated';
+  }
+  orderExist (id) {
+    try {
+      if(this.parcels.find( parcels => parcels.userid == id).length > 0 ){
+        return true;
+      }
+      return false;
+    }catch(err){
+      return false;
+    }
   }
 }
