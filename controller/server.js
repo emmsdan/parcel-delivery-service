e2d886449be5ad7f313019c5918da5d56882a7e7
unknown <ecomje@gmail.com>
2018-11-09 19:52:34 -0800

    updated

--- controller/server.js
+++ controller/server.js
@@ -2,8 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import userRouter from  './routes/users.route'
-import adminRouter from  './routes/parcel.route'
-import authRouter from  './routes/authenicator.route'
 import parcelRouter from  './routes/parcel.route'
 import errorRouter from  './routes/error.route'
 
@@ -20,8 +18,6 @@ app.get('/', (req, res) => {
 })
 
 app.use ('/api/v1/users', userRouter);
-app.use ('/api/v1/admin', adminRouter);
-app.use ('/api/v1/access', authRouter);
 app.use ('/api/v1/parcel', parcelRouter);
 
 // handles error pages like 404
