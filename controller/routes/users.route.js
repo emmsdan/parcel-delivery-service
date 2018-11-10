479091fc81f101a512599bf747d090b84f64b9f6
unknown <ecomje@gmail.com>
2018-11-09 20:41:11 -0800

    usercan access all parcels

--- controller/routes/users.route.js
+++ controller/routes/users.route.js
@@ -1,5 +1,6 @@
 import express from'express';
 const userRouter = express.Router();
+import parcels from '../models/parcel.model'
 
 // get user profile.
 userRouter.get('/',  ( req, res ) => {
@@ -8,7 +9,7 @@ userRouter.get('/',  ( req, res ) => {
 
 // get user parcels
 userRouter.get('/:userId/parcels', ( req, res ) => {
-
+  res.json(parcels.getUserParcels(req.params.userId));
 });
 
 module.exports = userRouter;
