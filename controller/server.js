eaa0751190199edf72e91264f5d3334f7db1c250
Emmanuel Daniel <ecomje@gmail.com>
2018-11-16 11:39:01 -0800

    updated landing page message, and setup url to routes used

--- controller/server.js
+++ controller/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import userRouter from  './routes/users.route'
-import parcelRouter from  './routes/parcel.route'
-import errorRouter from  './routes/error.route'
+import userRouter from  './routes/users'
+import parcelRouter from  './routes/parcel'
+import errorRouter from  './routes/error'
 
 const app = express();
 
@@ -14,29 +14,8 @@ if (port == null || port == "") {
 }
 
 app.get('/', (req, res) => {
-  res.send(`
-  <link rel='stylesheet' href='https://emmsdan.github.io/parcel-delivery-service/UI/assets/css/main.css' />
-  <h2> create user </h2>
-  <form action="/api/v1/users/create" method="post">
-    <input name="name"> name <br>
-    <input name="email"> email <br>
-    <input name="phone"> phone <br>
-    <input name="password"> password <br><br>
-      <button type='submit'> submit </button>
-  </form>
-  <br/>
-  <br/>
-  <h2> create parcel </h2>
-  <form action="/api/v1/parcel" method="post">
-    <input name="userid"> userid <br>
-    <input name="content"> content <br>
-    <input name="pickup"> pickup <br>
-    <input name="pickupcode"> pickupcode <br>
-    <input name="destination"> destination <br>
-    <input name="destinationcode"> destinationcode <br>
-    <input name="weight"> weight <br><br>
-      <button type='submit'> submit </button>
-  </form>`)
+  res.json({'message': `Please use the proper API version to access the page`})
+  res.status(401)
 })
 
 app.use ('/api/v1/users', userRouter);
