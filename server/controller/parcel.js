dbadd70987a9e45c32cee5d11677458d969a4d17
Emmanuel Daniel <ecomje@gmail.com>
2018-11-21 14:39:33 -0800

    updated eslint, and jsdoc

--- server/controller/parcel.js
+++ server/controller/parcel.js
@@ -1,65 +1,97 @@
-import {inArray, removeArray, generateID} from '../helpers/helper';
+import { inArray, generateID } from '../helpers/helper';
 
+/**
+ * Handles Parcel Orders
+ * @author EmmsDan <ecomje@gmail.com>
+ */
 class ParcelOrder {
-  constructor (parcels) {
+  /**
+   *  Assigns data to the program
+   * @param {array} parcels
+   */
+  constructor(parcels) {
     this.parcels = parcels;
   }
-  setStatus (statusCode) {
+
+  /**
+   *  set response status
+   * @param {number} statusCode : set response status
+   */
+  setStatus(statusCode) {
     this.statusCode = statusCode;
   }
-  status () {
+
+  /**
+   * Get response status
+   * @returns {number}
+   */
+  status() {
     return this.statusCode;
   }
-  getParcels () {
+
+  /**
+   * Get access to all the Parcels in DB
+   * @returns {object}
+   */
+  getParcels() {
     if (this.parcels.length === 0) {
-      this.setStatus(404)
-      return {'message': 'Parcel not available'};
+      this.setStatus(404);
+      return { message: 'Parcel not available' };
     }
-    this.setStatus(200)
+    this.setStatus(200);
     return (this.parcels);
   }
 
-  getSingleParcel (id) {
+  /**
+   *  get a Specific parcel
+   * @param {number} id
+   * @returns {array}
+   */
+  getSingleParcel(id) {
     return inArray(this.parcels, 'id', id);
   }
 
-  getUserParcels (userid) {
-    const parcel = this.parcels.find( parcels => parcels.userid == userid);
-    if(parcel){
-      this.setStatus(200)
+  /**
+   *  Get Specific order relating a User
+   * @param {string/number} userid
+   * @returns {array/object}
+   */
+  getUserParcels(userid) {
+    const parcel = this.parcels.find(parcels => parcels.userid === userid);
+    if (parcel) {
+      this.setStatus(200);
       return parcel;
-    }else{
-      this.setStatus(404)
-      return {'message':'Parcel not available for This User'};
     }
+    this.setStatus(404);
+    return { message: 'Parcel not available for This User' };
   }
 
-  removeParcel (parcelId) {
-    const deleteOrder= this.parcels.find(p => p.id === parseInt(parcelId))
-    if (!deleteOrder){
-      this.setStatus(404)
-      return {'message' : 'The Order, you are looking for, does not exist'};
-    } else {
-      this.setStatus(410)
-      this.parcels.splice(this.parcels.indexOf(deleteOrder), 1)
-      return {'message': 'This Order has been deleted'};
-    }
-  }
 
-  createOrder (order) {
+  /**
+   *  Create Parcel delivery order
+   * @param {object} order
+   * @returns {object}
+   */
+  createOrder(order) {
     let orderId = generateID(999);
-    if (this.orderExist(orderId)){
+    if (this.orderExist(orderId)) {
       orderId = (this.parcels - 1) + (orderId * 2);
     }
-    console.log (order)
-    if (order.userId === undefined){
-      return {'message' : 'UserId not specified: Order Must belong to a User'};
+
+    if (order.userId === undefined) {
+      return { message: 'UserId not specified: Order Must belong to a User' };
     }
-    if (order.content === undefined || order.pickup === undefined || order.pickupcode === undefined || order.destination === undefined || order.destinationcode === undefined || order.weight === undefined){
-      return {'message' : 'Order Details can\'t be empty'};
+    if (order.content === undefined || order.pickup === undefined) {
+      return { message: 'Order Details can\'t be empty' };
+    }
+    if (order.destination === undefined || order.destinationcode === undefined) {
+      return { message: 'Order Details can\'t be empty' };
+    }
+    if (order.pickupcode === undefined || order.weight === undefined) {
+      return { message: 'Order Details can\'t be empty' };
     }
 
-    this.parcels.push ({
+    this.parcels.push({
       userid: order.userId,
       id: orderId,
       content: order.content,
@@ -68,42 +100,61 @@ class ParcelOrder {
       destination: order.destination,
       destinationcode: order.destinationcode,
       weight: order.weight,
-      status : 'pending'
+      status: 'pending'
     });
-    this.setStatus (201);
-    return {'message' : 'new order created'}
+    this.setStatus(201);
+    return { message: 'new order created' };
   }
-  updateParcel (id, field, value = null){
-    if (value !== null){
+
+  /**
+   * updated parcel status or destination
+   * @param {number} id
+   * @param {object} field
+   * @param {string} value
+   * @returns {object}
+   */
+  updateParcel(id, field, value = null) {
+    if (value !== null) {
       this.getSingleParcel(id).field = value;
-      return {'message': 'status has been updated'};
+      return { message: 'status has been updated' };
     }
-    const parcel = this.getSingleParcel (id);
+    const parcel = this.getSingleParcel(id);
     parcel.destination = field.destination;
     parcel.destinationcode = field.destinationcode;
-    return {'message': 'destination has been updated'};
+    return { message: 'destination has been updated' };
   }
-  cancelParcel (id){
-    const parcel = this.getSingleParcel (id);
-    if (typeof parcel === 'object'){
-      if (parcel.status === 'transit' && parcel.status === 'delivered'){
-        return {'message' : `This parcel cannot be canceled, Its status is ${parcel.status}`}
+
+  /**
+   * cancel parcel order from being delivered
+   * @param {number} id : parcel id
+   * @returns {object}
+   */
+  cancelParcel(id) {
+    const parcel = this.getSingleParcel(id);
+    if (typeof parcel === 'object') {
+      if (parcel.status === 'transit' && parcel.status === 'delivered') {
+        return { message: `This parcel cannot be canceled, Its status is ${parcel.status}` };
       }
       parcel.status = 'canceled';
-      return {'message': 'This parcel has be canceled'};
+      return { message: 'This parcel has be canceled' };
     }
-    return {'message': 'parcel not found'}
+    return { message: 'parcel not found' };
   }
-  orderExist (id) {
+
+  /**
+   *  Checks if parcel order exist
+   * @param {number} id : parcel id
+   * @returns {boolean}
+   */
+  orderExist(id) {
     try {
-      if(this.parcels.find( parcels => parcels.userid == id)){
+      if (this.parcels.find(parcels => parcels.userid === id)) {
         return true;
       }
       return false;
-    }catch(err){
+    } catch (err) {
       return false;
     }
   }
 }
-
-export default ParcelOrder
+export default ParcelOrder;
