fdb0c5f521d25b258302a776089769a03c4d33fe
Emmanuel Daniel <ecomje@gmail.com>
2018-11-20 17:44:49 -0800

    updated decodeAuthToken to return callback

--- controller/models/v2AuthToken.js
+++ controller/models/v2AuthToken.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import jwt  from 'jsonwebtoken';
-
+process.env.TOKEN_SECRET = '\xf8%\xa8\xf2INz\xcc:\x171\xeei\x82\xce\x81Y\xc2HJ\xe5\x01\xf3$';
 const encodeAuthToken = (user) => {
   const playload = {
     exp: moment().add(14, 'days').unix(),
@@ -10,15 +10,14 @@ const encodeAuthToken = (user) => {
   return jwt.sign(playload, process.env.TOKEN_SECRET);
 }
 const decodeAuthToken = (token, callback) => {
+  const payload = jwt.decode(token);
   const now = moment().unix();
   // check if the token has expired
-  const payload = jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      callback('Token has expired.')
-    }else {
-      callback(null, payload);
-    }
-  });
+  if (now > payload.exp){
+    callback('Token has expired.');
+  } else {
+    callback(null, payload);
+  }
 }
 module.exports = {
   decodeAuthToken,
