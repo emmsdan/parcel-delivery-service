dbd67d2227ae69469c27d53230fc9b9ede103be0
Emmanuel Daniel <ecomje@gmail.com>
2018-11-22 06:42:58 -0800

    refactored code

--- server/helpers/helper.js
+++ server/helpers/helper.js
@@ -1,14 +1,13 @@
-export const newDate = () => new Date().toString()
-
 export const inArray = (array, id, value) => {
-    let row = array.find(( r ) => {
-      return r.id == value
-    });
-    if (!row) {
-      return 'item does not exist';
-    }
-    return (row)
-}
-export const generateID = (max) => {
-  return Math.floor(Math.random() * max  + 100);
-}
+  const row = array.find(r => r.id === value);
+  if (!row) {
+    return 'item does not exist';
+  }
+  return (row);
+};
+
+export const generateID = (max) => { return Math.floor(Math.random() * max + 100); };
+
+export const isEmpty = (obj) => {
+  return Object.keys(obj).length === 0;
+};
