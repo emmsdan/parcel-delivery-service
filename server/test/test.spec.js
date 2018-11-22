03bff26abf0e234e508776691fc2c448d67a323a
Emmanuel Daniel <ecomje@gmail.com>
2018-11-21 17:24:00 -0800

    updated to fix eslint

--- server/test/test.spec.js
+++ server/test/test.spec.js
@@ -1,45 +1,45 @@
 import supertest from 'supertest';
-import chai from 'chai'
-import app from "../app";
+import chai from 'chai';
+import app from '../app';
 
 global.app = app;
 global.expect = chai.expect;
 global.request = supertest(app);
 
-describe ( 'Parcel API Routes', () => {
-  beforeEach( (done) => {
-      done();
+describe('Parcel API Routes', () => {
+  beforeEach((done) => {
+    done();
   });
 
   describe('POST /api/v1/parcel', () => {
     it('should create parcels order successfully', (done) => {
       request.post('/api/v1/parcel')
-      .type ('form')
-      .send ({
-        userId: 3244,
-        content : 'iphone 6+ with hp 665 notepad',
-        weight : 23,
-        pickup : 'Ikeja, Lagos Nigeria',
-        destination : 'Ojota, Lagos Nigeria',
-        destinationcode: 123
-      })
-      .set('Accept', /application\/json/)
-      .expect(200)
-      .end( (err, res) => {
-        expect(res.body.message).equal('new order created')
+        .type('form')
+        .send({
+          userId: 3244,
+          content: 'iphone 6+ with hp 665 notepad',
+          weight: 23,
+          pickup: 'Ikeja, Lagos Nigeria',
+          destination: 'Ojota, Lagos Nigeria',
+          destinationcode: 123
+        })
+        .set('Accept', /application\/json/)
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.message).equal('new order created');
           done(err);
-      });
+        });
     });
   });
 
   describe('GET /api/v1/parcel', () => {
     it('should return list of parcels as an array ', (done) => {
       request.get('/api/v1/parcel')
-      .expect(200)
-      .end( (err, res) => {
-        expect(res.body).to.a('array')
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body).to.a('array');
           done(err);
-      });
+        });
     });
   });
 
@@ -47,16 +47,16 @@ describe ( 'Parcel API Routes', () => {
   describe('GET /api/v1/parcel/:parcelID', () => {
     it('should get single parcel order', (done) => {
       request.get ('/api/v1/parcel')
-      .expect(200)
-      .end ( (err, res ) => {
-        request.get(`/api/v1/parcel/${res.body[0].id}`)
         .expect(200)
-        .end( (err, res) => {
-          expect(res.body)
-          .to.a('object')
-            done(err);
+        .end((err, res ) => {
+          request.get(`/api/v1/parcel/${res.body[0].id}`)
+            .expect(200)
+            .end( (err, res) => {
+              expect(res.body)
+                .to.a('object');
+              done(err);
+            });
         });
-      })
     });
   });
 
