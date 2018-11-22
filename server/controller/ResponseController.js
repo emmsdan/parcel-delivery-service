13c24e7ab7e7a5ded630c601dd38307b4efa6923
Emmanuel Daniel <ecomje@gmail.com>
2018-11-21 17:42:21 -0800

    Route Response is now well defined and Organized

---
+++ server/controller/ResponseController.js
@@ -0,0 +1,61 @@
+import validator from 'validator';
+
+const response = Symbol('response');
+const statusCode = Symbol('statusCode');
+
+/**
+ * v2Response.js:
+ * Handles simple response.
+ * can set response status, response message (string or array)
+ * make ref to: #162088549 on PT.
+ */
+class ResponseController {
+  /**
+   *  no value is to be pasted to constructor,
+   * since the class is to be inherited
+   */
+  constructor() {
+    if (new.target === ResponseController) {
+      throw new Error('Class Can\'t can be Instantiated');
+    }
+    this[response] = [];
+    this[statusCode] = 200;
+  }
+
+  /**
+   * set the response status
+   * @param {number} code
+   */
+  setStatus(code) {
+    if (validator.isNumeric(code)) {
+      this[statusCode] = code;
+    }
+  }
+
+  /**
+   * set response message
+   * @param {strin/object} resp
+   * @param {null/string} type
+   */
+  setResponse(resp, type = null) {
+    this[response] = ((type === null && typeof resp !== 'string') ? { status: this.status(), data: [resp] } : { status: this.status(), error: resp });
+  }
+
+  /**
+   * get response
+   * @returns {array};
+   */
+  response() {
+    return this[response];
+  }
+
+  /**
+   * get response status
+   * @returns {number}
+   */
+  status() {
+    return this[statusCode];
+  }
+}
+
+export default ResponseController;
