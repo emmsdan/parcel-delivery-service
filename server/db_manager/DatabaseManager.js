e19527735fcf066b429f0fefacfdd3cea9b770c5
Emmanuel Daniel <ecomje@gmail.com>
2018-11-21 16:03:08 -0800

    this class will handle all database manipulation

---
+++ server/db_manager/DatabaseManager.js
@@ -0,0 +1,37 @@
+import { Client } from 'pg';
+
+const dbURL = 'postgres://localhost:5432/sendItDb';
+
+/**
+ * Handles database manipulations
+ */
+class DatabaseManager {
+  /**
+   * setup database
+   * @param {string} sql
+   * @param {string/object/array} fields (optional)
+   * @param {string} dbUrl (optional)
+   * @returns {promise}
+   */
+  static query(sql, fields = null, dbUrl = dbURL) {
+    const client = new Client(dbUrl);
+    client.connect();
+    if (fields === null) {
+      return client.query(sql);
+    }
+    return client.query(sql, fields);
+  }
+
+  /**
+   *  handles db client
+   * @param {string} dbUrl (optional)
+   * @returns {object}
+   */
+  static client(dbUrl = dbURL) {
+    const client = new Client(dbUrl);
+    client.connect();
+    return client;
+  }
+}
+
+module.exports = DatabaseManager;
