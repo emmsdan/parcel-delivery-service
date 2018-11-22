abff4ab5c8b4f5e3b3e95fb160b0cda1ead8f3c1
Emmanuel Daniel <ecomje@gmail.com>
2018-11-22 06:41:23 -0800

    added megration script, included bodyparser to default route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 
 import defaultRouter from './server/routes/defaultRoute';
 import errorRouter from './server/routes/errorRoute';
+import DatabaseManager from './server/db_manager/DatabaseManager';
 
 const app = express();
 
@@ -17,12 +18,53 @@ app.get('/', (req, res) => {
   res.status(401);
 });
 
-app.use('/api/v1/', defaultRouter);
+app.use('/api/v1/', bodyParser.urlencoded({ extended: false }), defaultRouter);
 
 // handles error pages like 404
 app.use('*', errorRouter);
 
-const server = app.listen(port, () => console.log(`App server is listening on port ${port}!`));
+/**
+ * start listen to server
+ * Create need tables on server start
+ */
+const server = app.listen(port, () => {
+  DatabaseManager.query(`CREATE TABLE  IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    userId VARCHAR,
+    username VARCHAR,
+    fullname VARCHAR,
+    phone Numeric,
+    email VARCHAR  (60) UNIQUE,
+    sex VARCHAR,
+    password VARCHAR,
+    registered TIMESTAMP,
+    isAdmin Varchar
+  );
+  CREATE TABLE  IF NOT EXISTS parcel (
+    id SERIAL PRIMARY KEY,
+    userId VARCHAR,
+    pName VARCHAR,
+    pDesc VARCHAR,
+    pPix Varchar,
+    weight Numeric,
+    weightmetric VARCHAR,
+    status VARCHAR,
+    sentOn timestamp,
+    deliveredOn timestamp,
+    pickUpName VARCHAR,
+    pickUpAddress TEXT,
+    destName VARCHAR,
+    destAddress TEXT
+  )`)
+    .then((response) => {
+      console.log('Table Created/Exist');
+    })
+    .catch((error) => {
+      console.log(`Error Message: ${error}`);
+    });
+  console.log(`App server is listening on port ${port}!`);
+});
+
 
 process.on('exit', () => server.close());
 process.on('SIGTERM', () => server.close());
