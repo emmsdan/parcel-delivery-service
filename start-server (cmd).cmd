09240689286ebd9c032f8675d722b036f90e7ded
Emmanuel Daniel <ecomje@gmail.com>
2018-12-23 00:39:58 +0100

    added simple batch script to start development server directly with just a click

---
+++ start-server (cmd).cmd
@@ -0,0 +1,9 @@
+@echo off
+echo ParcelDelivery.io
+echo -----A courier service that helps users deliver parcels to different destinations.
+
+echo Author: @EmmsDan
+
+echo This Terminal is for Development Use Only
+
+npm start
